34400058dfc1e72482ee67c7b09fc00f4d18b500
Slobodan Markovic <slobodan.markovic216@gmail.com>
2024-07-13 16:49:03 +0200

    Removing depricated ts-jest from global setting, changing node.js.yml only to run on Node 18.x

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   preset: 'ts-jest/presets/default-esm',
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
