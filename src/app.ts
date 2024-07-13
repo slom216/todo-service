9fcfceca5cd3406867058f580796fe0b09a89da0
Slobodan Markovic <slobodan.markovic216@gmail.com>
2024-07-13 13:59:45 +0200

    Adding helmet to set HTTP response headers

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application } from 'express'
 import bodyParser from 'body-parser'
+import helmet from 'helmet'
 
 import router from './routes'
 import { errorMiddleware } from './middleware'
 
 const app: Application = express()
+
+app.use(helmet())
 
 app.use(bodyParser.json())
 
